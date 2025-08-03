06accf1257e19f1971b3adb4026f36f4acb3bade
Sneha Saxena <164117846+SnehaaSaxena@users.noreply.github.com>
2025-08-03 20:39:59 +0530

    Update sort-0s-1s-and-2s.java

--- Difficulty: Medium/Sort 0s, 1s and 2s/sort-0s-1s-and-2s.java
+++ Difficulty: Medium/Sort 0s, 1s and 2s/sort-0s-1s-and-2s.java
@@ -1,18 +1,37 @@
 class Solution {
     public void sort012(int[] arr) {
-        // code here
-        TreeMap<Integer,Integer> map=new TreeMap<>();
+       //APPROACH 1
+        // TreeMap<Integer,Integer> map=new TreeMap<>();
+        // for(int i=0;i<arr.length;i++){
+        //     map.put(arr[i],map.getOrDefault(arr[i],0)+1);
+        // }
+        // int j=0;
+        // for(int i:map.keySet()){
+        //     int value=map.get(i);
+        //     while(value>0){
+        //         arr[j]=i;
+        //         j++;
+        //         value--;
+        //     }
+        // }
+        
+        //APPROACH 2
+        int max=Integer.MIN_VALUE;
         for(int i=0;i<arr.length;i++){
-            map.put(arr[i],map.getOrDefault(arr[i],0)+1);
+            max=arr[i]>max ? arr[i] : max;
+        }
+        
+        int freq[]=new int[max+1];
+        for(int i=0;i<arr.length;i++){
+            freq[arr[i]]++;
         }
         
         int j=0;
-        for(int i:map.keySet()){
-            int value=map.get(i);
-            while(value>0){
+        for(int i=0;i<freq.length;i++){
+            while(freq[i]>0){
                 arr[j]=i;
                 j++;
-                value--;
+                freq[i]--;
             }
         }
     }
