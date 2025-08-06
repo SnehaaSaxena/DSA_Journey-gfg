50df8aa5f671f37ce362748b2f585c7379b31434
Sneha Saxena <164117846+SnehaaSaxena@users.noreply.github.com>
2025-08-07 01:44:06 +0530

    Update overlapping-intervals.java

--- Difficulty: Medium/Overlapping Intervals/overlapping-intervals.java
+++ Difficulty: Medium/Overlapping Intervals/overlapping-intervals.java
@@ -1,25 +1,40 @@
 class Solution {
     public ArrayList<int[]> mergeOverlap(int[][] arr) {
         // Code here
+       // APPROACH 1
+        // ArrayList<int [] > list=new ArrayList<>();
+        // Arrays.sort(arr,Comparator.comparingDouble(o -> o[0]));
+        // int start=0,end=0;
+        
+        // for(int i=0;i<arr.length;i++){
+        //     start=arr[i][0];end=arr[i][1];
+            
+        //     if(!list.isEmpty() && end<=list.get(list.size()-1)[1])
+        //       continue;
+             
+        //   for(int j=i+1;j<arr.length;j++){
+        //       if(end>=arr[j][0])
+        //           end=Math.max(end,arr[j][1]);
+        //       else
+        //           break;
+        //   }
+        //   int li[]=new int[]{start,end};
+        //   list.add(li);
+        // }
+        // return list;
+        
+        
+        //APPROACH 2
         ArrayList<int [] > list=new ArrayList<>();
         Arrays.sort(arr,Comparator.comparingDouble(o -> o[0]));
-        int start=0,end=0;
         
         for(int i=0;i<arr.length;i++){
-            start=arr[i][0];end=arr[i][1];
-            
-            if(!list.isEmpty() && end<=list.get(list.size()-1)[1])
-              continue;
-             
-          for(int j=i+1;j<arr.length;j++){
-              if(end>=arr[j][0])
-                  end=Math.max(end,arr[j][1]);
-              else
-                  break;
-          }
-          int li[]=new int[]{start,end};
-          list.add(li);
+            if( list.isEmpty() || arr[i][0] > list.get(list.size()-1)[1])
+                list.add(arr[i]);
+            else
+                list.get(list.size()-1)[1]=Math.max(arr[i][1],list.get(list.size()-1)[1]);
         }
-        return list;
+        
+       return list; 
     }
 }
