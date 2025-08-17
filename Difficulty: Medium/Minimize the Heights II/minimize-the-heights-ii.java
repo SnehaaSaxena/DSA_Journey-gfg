5dbe7451142bb6d574cf598e31d744bcc0820b2b
Sneha Saxena <164117846+SnehaaSaxena@users.noreply.github.com>
2025-08-17 13:40:15 +0530

    Update minimize-the-heights-ii.java

--- Difficulty: Medium/Minimize the Heights II/minimize-the-heights-ii.java
+++ Difficulty: Medium/Minimize the Heights II/minimize-the-heights-ii.java
@@ -13,3 +13,6 @@ class Solution {
        return diff;
     }
 }
+
+// 1,5,8,10  k=2-> min=1 , max=10 , diff=9 we need to find smallest diff so min+k and max-k 1+2=3 and 10-2=8 now min=3 and max=8 we are increasing the min i.e 
+// arr[i-1] so compare it with max  and we are decreasing the max i.e arr[i] so comapre it with min
