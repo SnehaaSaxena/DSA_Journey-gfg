aa322467cd85611197e8d381501828af14d86879
Sneha Saxena <164117846+SnehaaSaxena@users.noreply.github.com>
2025-08-18 23:47:02 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Rearrange Array Alternately/rearrange-array-alternately.java
+++ Difficulty: Medium/Rearrange Array Alternately/rearrange-array-alternately.java
@@ -1,18 +1,39 @@
+// class Solution {
+//     public void rearrange(int arr[]) {
+//         int nums[]= Arrays.copyOfRange(arr, 0, arr.length);
+//         Arrays.sort(nums);
+//         int i=0,j=nums.length-1,k=0;
+//         while(i<=j){
+//             if(i==j){
+//               arr[k++]=nums[i];
+//               i++;j--;
+//             }
+//             else{
+//               arr[k++]=nums[j];
+//               arr[k++]=nums[i];
+//               i++;j--;
+//             }
+//          }
+//       }
+// }
+
+// APPROACH 2 (full explanantion in notes)
+// [1,2,3,4,5,6] -> MAX value element-even index and MIN value element-odd index
 class Solution {
     public void rearrange(int arr[]) {
-        int nums[]= Arrays.copyOfRange(arr, 0, arr.length);
-        Arrays.sort(nums);
-        int i=0,j=nums.length-1,k=0;
-        while(i<=j){
-            if(i==j){
-              arr[k++]=nums[i];
-              i++;j--;
+        Arrays.sort(arr);
+        int max_index=arr.length-1, min_index=0, max=arr[arr.length-1]+1;
+        for(int i=0;i<arr.length;i++){
+            if(i%2==0){
+                arr[i]=(arr[max_index]%max)*max+arr[i];
+                max_index--;
+            }else{
+                 arr[i]=(arr[min_index]%max)*max+arr[i];
+                 min_index++;
             }
-            else{
-              arr[k++]=nums[j];
-              arr[k++]=nums[i];
-              i++;j--;
-            }
-         }
+        }
+        for(int i=0;i<arr.length;i++){
+            arr[i]/=max;
+        }
       }
 }
