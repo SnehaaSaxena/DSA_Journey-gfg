98e9e1c3f6cb42811d0c97ee4560d67598cda40b
Sneha Saxena <164117846+SnehaaSaxena@users.noreply.github.com>
2025-08-30 22:26:49 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Row with max 1s/row-with-max-1s.java
+++ Difficulty: Medium/Row with max 1s/row-with-max-1s.java
@@ -2,18 +2,31 @@
 
 class Solution {
     public int rowWithMax1s(int arr[][]) {
-        int count, maxCount=Integer.MIN_VALUE,row=-1;
-      for(int i=0;i<arr.length;i++){
-           count=0;
-          for(int j=0;j<arr[0].length;j++){
-              if(arr[i][j]==1)
-               count++;
-          }
-          if(count>maxCount){
-            maxCount=count;
-            row=i;
-          }
-      }
-      return row;
+    //     int count, maxCount=Integer.MIN_VALUE,row=-1;
+    //   for(int i=0;i<arr.length;i++){
+    //       count=0;
+    //       for(int j=0;j<arr[0].length;j++){
+    //           if(arr[i][j]==1)
+    //           count++;
+    //       }
+    //       if(count>maxCount){
+    //         maxCount=count;
+    //         row=i;
+    //       }
+    //   }
+    //   return row;
+    
+    
+    //APPROACH 2 (it says if we found 1 then all the elements after that are also 1 because it ia a sorted matrix)
+    int left=0,right=arr[0].length-1,res=-1;
+    while(left<arr.length && right>=0){
+        if(arr[left][right]==1){
+            res=left;
+            right--;
+        }else
+            left++;
     }
+     return res;
+  }
 }
+
