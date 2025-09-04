394913dd6fb39a1fcbb5a768098c71df159e1b03
Sneha Saxena <164117846+SnehaaSaxena@users.noreply.github.com>
2025-09-04 17:14:39 +0530

    Update kth-smallest.java

--- Difficulty: Medium/Kth Smallest/kth-smallest.java
+++ Difficulty: Medium/Kth Smallest/kth-smallest.java
@@ -1,9 +1,25 @@
-// User function Template for Java
-
 class Solution {
     public static int kthSmallest(int[] arr, int k) {
-       Arrays.sort(arr);
-       return arr[k-1];
-        
+       //APPROACH 1
+       //Arrays.sort(arr);
+       //return arr[k-1];
+       
+       //APPROACH 2
+       int max=Integer.MIN_VALUE;
+       for(int i=0;i<arr.length;i++){
+           max=arr[i]>max ? arr[i]:max;
+       }
+       int freq[]=new int[max+1];
+       for(int i=0;i<arr.length;i++){
+           freq[arr[i]]++;
+       }
+       for(int i=0;i<freq.length;i++){
+           if(freq[i]>0){
+               k--;
+               if(k==0)
+                 return i;
+           }
+       }
+       return -1;
     }
 }
