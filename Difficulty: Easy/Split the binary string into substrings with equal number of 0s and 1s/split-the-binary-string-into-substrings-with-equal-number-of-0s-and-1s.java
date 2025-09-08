4104d6aafdef976c07bfc973471e69989d7912bf
Sneha Saxena <164117846+SnehaaSaxena@users.noreply.github.com>
2025-09-08 20:27:42 +0530

    Update split-the-binary-string-into-substrings-with-equal-number-of-0s-and-1s.java

--- Difficulty: Easy/Split the binary string into substrings with equal number of 0s and 1s/split-the-binary-string-into-substrings-with-equal-number-of-0s-and-1s.java
+++ Difficulty: Easy/Split the binary string into substrings with equal number of 0s and 1s/split-the-binary-string-into-substrings-with-equal-number-of-0s-and-1s.java
@@ -1,18 +1,29 @@
-// User function Template for Java
-
 class Solution {
     public static int maxSubStr(String str) {
-        int oneC=0,zeroC=0,totalCount=0;
+        // int oneC=0,zeroC=0,totalCount=0;
+        // for(int i=0;i<str.length();i++){
+        //     char ch=str.charAt(i);
+        //     if(ch=='1') oneC++;
+        //     else zeroC++;
+        //     if(oneC==zeroC){
+        //       totalCount++;
+        //       oneC=0;
+        //       zeroC=0;
+        //     } 
+        // }
+        // return oneC==zeroC ? totalCount : -1;
+        
+        // Another way
+        int sum=0,res=0;
         for(int i=0;i<str.length();i++){
             char ch=str.charAt(i);
-            if(ch=='1') oneC++;
-            else zeroC++;
-            if(oneC==zeroC){
-              totalCount++;
-              oneC=0;
-              zeroC=0;
-            } 
+            if(ch=='0') sum-=1;
+            else sum+=1;
+            if(sum==0) res++;
         }
-        return oneC==zeroC ? totalCount : -1;
+        if(sum!=0){
+            return -1;
+        } 
+        return res;
     }
 }
