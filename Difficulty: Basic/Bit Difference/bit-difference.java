bb09ec258bc3a70a0cda2a0175ebafeb4a77cff4
Sneha Saxena <164117846+SnehaaSaxena@users.noreply.github.com>
2025-09-18 18:36:22 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Bit Difference/bit-difference.java
+++ Difficulty: Basic/Bit Difference/bit-difference.java
@@ -1,11 +1,27 @@
 class Solution {
     public static int countBitsFlip(int a, int b) {
+        // int num=a^b;
+        // int count=0;
+        // while(num!=0){
+        //   count++;
+        //   num=num & num-1;
+        // }
+        // return count;
+        
+        // ANOTHER WAY
         int num=a^b;
         int count=0;
-        while(num!=0){
-           count++;
-           num=num & num-1;
+        while(num>0){
+          int x=num%2;
+          if(x==1){
+            count++;
+           }
+           num=num/2;
+           if(num==1){
+               count++;
+               break;
+            }  
         }
-        return count;
+       return count; 
     }
 }
