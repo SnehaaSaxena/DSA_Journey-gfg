d046372f0b6154a865296a2f0bca529353689d54
Sneha Saxena <164117846+SnehaaSaxena@users.noreply.github.com>
2025-09-23 20:20:53 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Move all negative elements to end/move-all-negative-elements-to-end.java
+++ Difficulty: Easy/Move all negative elements to end/move-all-negative-elements-to-end.java
@@ -4,19 +4,25 @@
 
 class Solution {
     public void segregateElements(int[] arr) {
-      ArrayList<Integer> list=new ArrayList<>();
-      int j=0;
-      for(int i=0;i<arr.length;i++){
-          if(arr[i]<0)
-              list.add(arr[i]);
-          else
-             arr[j++]=arr[i];
-       }
+    //   ArrayList<Integer> list=new ArrayList<>();
+    //   int j=0;
+    //   for(int i=0;i<arr.length;i++){
+    //       if(arr[i]<0)
+    //           list.add(arr[i]);
+    //       else
+    //          arr[j++]=arr[i];
+    //   }
        
-       int n=arr.length;
-       for(int i=n-list.size();i<n;i++){
-           arr[i]=list.get(i-(n-list.size()));
-       }
+    //   int n=arr.length;
+    //   for(int i=n-list.size();i<n;i++){
+    //       arr[i]=list.get(i-(n-list.size()));
+    //   }
      
-    }
+     //ANOTHER WAY
+     int temp[]=new int[arr.length];
+     int j=0;
+     for(int i=0;i<arr.length;i++) if(arr[i]>=0) temp[j++]=arr[i];
+     for(int i=0;i<arr.length;i++) if(arr[i]<0) temp[j++]=arr[i];
+     for(int i=0;i<arr.length;i++)  arr[i]=temp[i];
+     }
 }
