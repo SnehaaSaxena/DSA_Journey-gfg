417113c87cd43e2938b4c2c991de3ee2ea20ed55
Sneha Saxena <164117846+SnehaaSaxena@users.noreply.github.com>
2025-10-26 01:02:43 +0530

    Update the-celebrity-problem.java

--- Difficulty: Medium/The Celebrity Problem/the-celebrity-problem.java
+++ Difficulty: Medium/The Celebrity Problem/the-celebrity-problem.java
@@ -15,5 +15,23 @@ class Solution {
             (knowMe[i]==mat.length && iKnow[i]==1) ) return i;
         }
       return -1;  
-    }
+
+
+        // two pointer approach
+          int top=0,down=mat.length-1;
+          while(top<down){
+              if(mat[top][down]==1) top++; //if a person knows someone then he is not the celebrity so move down
+              else if(mat[down][top]==1) down--; //if a person knows someone then he is not the celebrity so move up
+              else{ // this is case where neither of them can be a celebrity as to be a celebrity everyone should know them but here for each of them 1-1 person is reduced
+                  top++;
+                  down--;
+              }
+          }
+          for(int i=0;i<mat[top].length;i++){
+              if(top==i) continue;
+              else if(mat[top][i]==0 && mat[i][top]==1);
+              else return -1;
+          }
+          return top;
+    } 
 }
