02f999c95069bc3d3922012d5302fa53ccd10764
Sneha Saxena <164117846+SnehaaSaxena@users.noreply.github.com>
2026-02-10 02:23:39 +0530

    Change arraySum to recursive sum calculation
    Refactored arraySum method to use recursion for summing elements.

--- Difficulty: Basic/Sum of Array/sum-of-array.java
+++ Difficulty: Basic/Sum of Array/sum-of-array.java
@@ -2,10 +2,21 @@
 
 class Solution {
     int arraySum(int arr[]) {
-        int sum=0;
-        for(int i=0;i<arr.length;i++){
-            sum+=arr[i];
-        } 
-        return sum;
-     }
+        // int sum=0;
+        // for(int i=0;i<arr.length;i++){
+        //     sum+=arr[i];
+        // } 
+        // return sum;
+        
+        
+         // way 2 (RECURSIVE METHOD)
+         return findSum(arr,0);
+      }
+      
+      int findSum(int arr[], int i){
+          if(i==arr.length-1){
+              return arr[i];
+          }
+          return findSum(arr,i+1)+arr[i];
+      }
 }
