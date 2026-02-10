a0ab5aa8ddf49ee5ace47c892793a40d328217fa
Sneha Saxena <164117846+SnehaaSaxena@users.noreply.github.com>
2026-02-11 00:46:30 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Elements in the Range/elements-in-the-range.java
@@ -0,0 +1,18 @@
+// User function Template for Java
+
+class Solution {
+    boolean check_elements(int arr[], int n, int A, int B) {
+        for(int i=A;i<=B;i++){
+            boolean found=false;
+            for(int j=0;j<arr.length;j++){
+                if(arr[j]==i){
+                    found=true;
+                    break;
+                } 
+            }
+            if(!found) return false;
+        }
+        return true;
+        
+    }
+}
